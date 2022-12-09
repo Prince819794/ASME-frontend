d35713730e3aea322debf7dd9990a50d64766dc2
Prince819794 <saikiapp9@gmail.com>
2022-12-09 16:41:07 +0530

    t

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -8,6 +8,24 @@ const Events = () => {
         <div className="ev-f-head">Events</div>
         <div className="ev-head">Events</div>
       </div>
+      {/* <div className="wrapper">
+        <div className="card">
+          <div class="box"></div>
+          <div class="box2 overlay"></div>
+        </div>
+        <div className="card">
+          <div class="box"></div>
+          <div class="box2 overlay"></div>
+        </div>
+        <div className="card">
+          <div class="box"></div>
+          <div class="box2 overlay"></div>
+        </div>
+        <div className="card">
+          <div class="box"></div>
+          <div class="box2 overlay"></div>
+        </div>
+      </div> */}
     </div>
   );
 };
