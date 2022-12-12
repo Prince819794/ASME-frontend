2315450d5d378a726061e051eaf8acc80b8dd8d3
Nrittik Sarmah <nrittik43@gmail.com>
2022-12-12 19:37:38 +0530

    e-fest page completed

--- src/components/Competition/index.js
+++ src/components/Competition/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import hpvcMain from "../../assets/hpvc-main.png";
-import hpvcMain2 from "../../assets/hpvc-main-2.png";
+import Carousel from "../Carousel";
 import "./styles.css";
 
-const Competition = () => {
+const Competition = (props) => {
   return (
     <div>
       <div className="comp-main-img">
-        <img src={hpvcMain} alt="" />
+        <img src={props.mainImg} alt="" />
         <div className="comp-wrapper"></div>
         <div className="wr-cnt-1">
           <div
@@ -15,28 +14,23 @@ const Competition = () => {
               fontSize: "1.8rem",
               fontWeight: "600",
               marginBottom: "0.4rem",
+              textTransform: 'uppercase'
             }}
           >
-            HPVC
+            {props.title}
           </div>
-          <div>Human Powered Vehicle Challenge</div>
+          <div>{props.description}</div>
         </div>
         <div className="wr-cnt-2">
-          <img src={hpvcMain2} alt="" />
+          <img src={props.subImg} alt="" />
         </div>
       </div>
       <div className="comp-body">
         <div className="comp-content">
-          ASME's e-Human Powered Vehicle Challenge (e-HPVC), sponsored by
-          Altair, is an engineering design and innovation competition that gives
-          students the opportunity to network and apply engineering principles
-          through the design, fabrication, and racing of human powered vehicles.
-          <div style={{ marginTop: "2rem" }}>
-            The first HPVC competition was in 1983 at the University of
-            California, Davis and was won by California State University, Chico.
-          </div>
+          {props.content1}
+          <div style={{ marginTop: "2rem" }}>{props.content2}</div>
         </div>
-        <div className="ci-carousel"></div>
+        <Carousel {...props} />
       </div>
     </div>
   );
