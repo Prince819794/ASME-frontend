c8ba7ffd06e97f6d7893470664da9b7a043db0be
Prince819794 <saikiapp9@gmail.com>
2022-12-14 10:39:16 +0530

    crescendo section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Cursor from "./components/Cursor";
+import Crescendo from "./pages/Crescendo";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/crescendo" element={<Crescendo />} />
       </Routes>
       {/* <Footer /> */}
     </>
---
+++ src/pages/Crescendo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import CrescendoBody from "../components/CrescendoBody";
+
+const Crescendo = () => {
+  return (
+    <>
+      {/* <Header /> */}
+
+      {/* tera component idhar bana.. comment ke place me  */}
+
+      <CrescendoBody />
+    </>
+  );
+};
+
+export default Crescendo;
